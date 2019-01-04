65fce13d232a41426674c475e0ae78e6dbad763a
Liam Perlaki <lperlaki@icloud.com>
2019-01-04 14:57:33 +0100

    added download function

--- helper.py
+++ helper.py
@@ -1,6 +1,23 @@
 import requests
 import gzip
 
+BASE = 'edengame.net'
+APP_BASE_URL = f'http://app.{BASE}'
+FILES_BASE_URL = f'http://files.{BASE}'
+UPLOAD_URL = f'{APP_BASE_URL}/upload2.php'
+REPORT_URL = f'{APP_BASE_URL}/report.php'
+POPULAR_URL = f'{FILES_BASE_URL}/popularlist.txt'
 
-def loadRaw(name):
-    return gzip.open(f'./{name}.eden', 'rb').read()
+
+def DOWNLOAD_MAP(id): return f'{FILES_BASE_URL}/{id}.eden'
+
+
+def DOWNLOAD_MAP_IMAGE(id): return f'{FILES_BASE_URL}/{id}.eden.png'
+
+
+def downloadRaw(id):
+    return requests.get(DOWNLOAD_MAP(id)).content
+
+
+def loadRaw(id):
+    return gzip.open(f'./{id}.eden', 'rb').read()
